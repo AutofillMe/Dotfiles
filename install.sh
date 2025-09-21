c6a6b54b2ca0ed909ad42ea3c7ece5e320733e3d
AutofillMe <113013764+AutofillMe@users.noreply.github.com>
2025-09-21 06:02:07 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,6 +81,7 @@ rm -rf /home/$username/Downloads/rmtrash/
 
 # Install btop theme
 curl -L https://github.com/catppuccin/btop/releases/latest/download/themes.tar.gz -o /home/$username/Downloads/btop.tar.gz
+mkdir /home/$username/Downloads/btop
 tar -xzf /home/$username/Downloads/btop.tar.gz -C /home/$username/Downloads/btop
 mkdir ~/.config/btop/themes
 mv /home/$username/Downloads/btop/themes/catppuccin_mocha.theme ~/.config/btop/themes/catppuccin_mocha.theme
