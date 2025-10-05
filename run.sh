00497ef63733ded448864fe3534ae32292282113
AutofillMe <113013764+AutofillMe@users.noreply.github.com>
2025-10-05 15:41:04 +0000

    add kill term on cleanup

--- run.sh
+++ run.sh
@@ -201,5 +201,17 @@ dnf_uninstall() {
 cleanup() {
 	echo "Running final cleanup..."
     # Final cleanup
-	echo "Please close then reopen your terminal, then run nvim and change the defaults."
+	echo "Please close then reopen your terminal, then run nvim and change the default theme."
+	sleep 3
+	echo "Closing terminal in 3 seconds"
+	sleep 1
+	echo "3..."
+	sleep 1
+	echo "2..."
+	sleep 1
+	echo "1..."
+	sleep 1
+	echo "Exiting..."
+	sleep 2
+	exit
 }
