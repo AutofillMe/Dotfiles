2c3564326f1265f8eb65397f8f4a40f888beff21
AutofillMe <113013764+AutofillMe@users.noreply.github.com>
2025-10-19 15:04:16 +0000

    update sed statements for neovim configs

--- post-install.sh
+++ post-install.sh
@@ -37,11 +37,32 @@ chsh -s /usr/bin/zsh
 
 # Fix NVChad configs and add python and c lsp and format
 sed -i '/-- event/c\    event = { "BufWritePre" },' $user_home/.config/nvim/lua/plugins/init.lua
-sed -i $'0,/{/s|{|{\\\n  {\\\n    "folke/todo-comments.nvim",\\\n    event = "VimEnter",\\\n    dependencies = { "nvim-lua/plenary.nvim" },\\\n    opts = { signs = false }\n  },|' $user_home/.config/nvim/lua/plugins/init.lua
+sed -i '0,/{/s|{|{\
+  {\
+    "folke/todo-comments.nvim",\
+    event = "VimEnter",\
+    dependencies = { "nvim-lua/plenary.nvim" },\
+    opts = { signs = false }\
+  },|' $user_home/.config/nvim/lua/plugins/init.lua
 sed -i 's/-- //' $user_home/.config/nvim/lua/configs/conform.lua
 sed -i '/html = { "prettier" },/a\        python = { "isort", "black" },\n\        c = { "clang-format" },\n\        cpp = { "clang-format" },\n\        sh = { "shfmt" },' $user_home/.config/nvim/lua/configs/conform.lua
 sed -i 's/"html", "cssls"/&, "pyright", "clangd"/' $user_home/.config/nvim/lua/configs/lspconfig.lua
 sed -i 's/onedark/catppuccin/' $user_home/.config/nvim/lua/chadrc.lua
+sed -i '0,/{/s|{|{\
+  formatters = {\
+        ["clang-format"] = {\
+            prepend_args = { "--style={IndentWidth: 4, TabWidth: 4}" },\
+        },\
+        prettier = {\
+            prepend_args = { "--use-tabs", "false", "--tab-width", "4" },\
+        },\
+        stylua = {\
+            prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },\
+        },\
+        shfmt = {\
+            prepend_args = { "-i", "4" },\
+        },\
+    }|' $user_home/.config/nvim/lua/configs/conform.lua
 
 # Remove bash files
 rm -f $user_home/.bash*
