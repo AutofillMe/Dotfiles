713bfbfba596def589adfb52311e6f33903cd73c
AutofillMe <113013764+AutofillMe@users.noreply.github.com>
2025-10-27 14:32:24 +0000

    change print tasks to loop func

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-script_version="1.0"
+validate_input() {
+	local tasks=("$@")
+
+	# Check if user selected any tasks at all
+	if [ ${#tasks[@]} -eq 0 ]; then
+		echo "No tasks selected. Exiting..."
+		exit 2
+	fi
+	
+    # Validate user input to be a number and between 0 and 12
+    local index
+    for index in "${tasks[@]}"; do
+        if ! [[ "$index" =~ ^[0-9]+$ ]] || (( index < 0 || index > 12 )); then
+            echo "Invalid task number: $index"
+            exit 3
+        fi
+    done
+}
+
+script_version="1.1"
 
 trap "echo 'Script interrupted. Exiting...'; exit 1" INT
 
@@ -47,26 +66,6 @@ checkpoint "Are you sure that you want to continue?"
 echo
 
 # Prompt which task checkpoint they would like to run
-echo "Which tasks would you like to execute?"
-echo "Provide the input as a spaced list of numbers or just type 0 for all tasks."
-echo "0) All Tasks"
-echo "1) copr_install"
-echo "2) dnf_install"
-echo "3) nerd_font_install"
-echo "4) font_check"
-echo "5) logout_delay"
-echo "6) rmtrash_install"
-echo "7) btop_theme_install"
-echo "8) NVChad_install"
-echo "9) tealdeer_update"
-echo "10) run_stow"
-echo "11) dnf_uninstall"
-echo "12) cleanup"
-read -p "Enter your choice: " -a selected_tasks
-
-# Validate input: check if it's a number between 0 and 12
-valid_input_checkpoint "${selected_tasks[@]}"
-
 task_functions=(
 	all_tasks
 	copr_install
@@ -82,6 +81,18 @@ task_functions=(
 	dnf_uninstall
 	cleanup
 )
+
+echo "Which tasks would you like to execute?"
+echo "Provide the input as a spaced list of numbers or just type 0 for all tasks."
+count=1
+for task in ${task_functions[@]}; do
+	echo "${count}) ${task}"
+	((count++)
+done
+read -rp "Enter your choice: " -a selected_tasks
+
+# Validate input: check if it's a number between 0 and 12
+validate_input "${selected_tasks[@]}"
 
 # If user selects 0 and other tasks, changes selected task to just 0 to reduce redundancy
 if [[ " ${selected_tasks[*]} " =~ " 0 " ]]; then
