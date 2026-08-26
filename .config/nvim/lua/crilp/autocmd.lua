003b69cfeca12d4f847e20647de8d02d6731998c
AutofillMe <113013764+AutofillMe@users.noreply.github.com>
2026-08-26 23:29:01 +0900

    Add BufDelete autocmd to handle empty buffer deletion

--- .config/nvim/lua/crilp/autocmd.lua
+++ .config/nvim/lua/crilp/autocmd.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
